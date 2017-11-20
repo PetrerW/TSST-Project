2ad408c79d6481c974ce2a71b22dfde2cbd0c867
Piotr Wieżel <piotrw76@gmail.com>
2017-11-20 17:00:24 +0100

    Klasa NetworkNode

---
+++ NetworkNode/EONTableRow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkNode
+{
+    /// <summary>
+    /// Rząd tabeli z systemu EON.
+    /// </summary>
+    class EONTableRow
+    {
+        /// <summary>
+        /// Na jakiej częstotliwości zaczyna się pasmo?
+        /// </summary>
+        public short busyFrequency { get; set; }
+
+        /// <summary>
+        /// Zajęte pasmo na wejściu węzła.
+        /// </summary>
+        public short busyBandIN { get; set; }
+        
+        /// <summary>
+        /// Zajęte pasmo na wyjściu węzła.
+        /// </summary>
+        public short busyBandOUT { get; set; }
+    }
+}
