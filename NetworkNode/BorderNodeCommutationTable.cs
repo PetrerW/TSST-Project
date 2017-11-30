9f37591890cc32274f7d6bb03e3bec26104c5ec4
Piotr Wieżel <piotrw76@gmail.com>
2017-11-30 03:45:38 +0100

    Komutacja w polu komutacji

---
+++ NetworkNode/BorderNodeCommutationTable.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkingTools;
+
+namespace NetworkNode
+{
+    /// <summary>
+    /// Klasa z tabelą komutacji węzłów brzegowych.
+    /// TODO: Jak dodajesz do listy nowy wiersz, to musisz sprawdzic, czy juz nie istnieje podobny wpis (z takim 
+    /// TODO: samym IP_IN i port_in)
+    /// </summary>
+    public class BorderNodeCommutationTable
+    {
+        /// <summary>
+        /// Tabela komutacji węzłów brzegowych
+        /// </summary>
+        public List<BorderNodeCommutationTableRow> Table { get; set; }
+
+        public BorderNodeCommutationTable()
+        {
+            Table = new List<BorderNodeCommutationTableRow>();
+        }
+
+        /// <summary>
+        /// Funkcja obliczajaca efektywnosc modulacji na podstawie ilosci hopow do celu, jaka modulacje ma uzyc.
+        /// </summary>
+        /// <param name="hopsNumber"></param>
+        public static short determineModulationPerformance(short hopsNumber)
+        {
+            try
+            {
+                if (hopsNumber <= 0)
+                    throw new Exception("hopsNumber is " + hopsNumber);
+                if (hopsNumber <= 8)
+                    return (short)(9 - hopsNumber);
+                else
+                    return 1;
+            }
+            //Jak sie cos nie uda to bierzemy modulacje o najmniejszej wydajnosci (N)
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// Funkcja obliczajaca potrzebne pasmo przy danej przeplynowsci bitowej i wydajnosci modulacji
+        /// </summary>
+        /// <param name="bitRate"></param>
+        /// <param name="modulationPerformance"></param>
+        /// <returns></returns>
+        public static short determineBand(short bitRate, short modulationPerformance)
+        {
+            //Maksymalna wydajnosc widmowa modulacji to 8bit/s/Hz
+            short maxLinkSpectrumPerformance = 8;
+
+            short band;
+
+            //Jak wartosciowosc modulacji nie przekracza maksymalnej wydajnosci widmowej lacza
+            if (modulationPerformance < maxLinkSpectrumPerformance)
+                band = (short)Math.Ceiling((double)bitRate / modulationPerformance);
+            else
+                //A jak przekracza, to dzielimy przez maksymalna wydajmosc widmowa lacza
+                band = (short)Math.Ceiling((double)bitRate / maxLinkSpectrumPerformance);
+
+            return band;
+        }
+
+        /// <summary>
+        /// Funkcja odnajdujaca rzad, w ktorym jest okreslone IP i nr portu
+        /// </summary>
+        /// <param name="IP_IN"></param>
+        /// <param name="port_in"></param>
+        /// <returns></returns>
+        public BorderNodeCommutationTableRow FindRow(string IP_IN, short port_in)
+        {
+            //Jeden rzad tablicy komutacji 
+            BorderNodeCommutationTableRow borderRow = null;
+
+            try
+            {
+                IPAddress IP = IPAddress.Parse(IP_IN);
+
+                //Wyszukiwanie takiego rzedu, ktory ma podane IP_IN i port_in
+                borderRow = this.Table.Find(row => (row.IP_IN.ToString() == IP_IN) && (row.port_in == port_in));
+
+                return borderRow;
+            }
+            catch (Exception E)
+            {
+                return borderRow;
+            }
+
+        }
+
+        /// <summary>
+        /// Funkcja służąca do zamiany nagłówków w wiadomościach zawartych w kolejne; Wiadomości pochodzą od klienta.
+        /// </summary>
+        /// <param name="q">Kolejka tablic bajtów</param>
+        /// <returns></returns>
+        public Queue<byte[]> changeHeaderForMessagesFromClient(Queue<byte[]> q)
+        {
+            Queue<byte[]> tempQueue = new Queue<byte[]>();
+            var tmp = q.ToArray();
+            int max = q.Count();
+            for (int i = 0; i < max; i++)
+            {
+                tmp[i] = this.changePackageHeader(tmp[i]);
+            }
+
+            for (int i = 0; i < max; i++)
+            {
+                tempQueue.Enqueue(tmp[i]);
+            }
+
+            return tempQueue;
+        }
+
+        /// <summary>
+        /// Funkcja słuząca do zamiany nagłówka 
+        /// </summary>
+        /// <param name="message">Wiadomość odebrana w postaci tablicy bajtów</param>
+        /// <returns></returns>
+        public byte[] changePackageHeader(byte[] message)
+        {
+            byte[] msg = new byte[64];
+            Package p = new Package(msg);
+
+            BorderNodeCommutationTableRow row = new BorderNodeCommutationTableRow();
+            row = this.FindRow(p.IP_Source.ToString(), p.portNumber);
+
+            p.changeBand(row.band);
+            p.changeFrequency(row.frequency);
+            p.changeModulationPerformance(row.modulationPerformance);
+            p.changeBitRate(row.bitRate);
+            p.changePort(row.Port);
+
+            msg = p.toBytes();
+
+            return msg;
+        }
+
+        /// <summary>
+        /// Funkcja odnajdujaca indeks rzedu o okreslony IP i porcie
+        /// </summary>
+        /// <param name="IP_IN"></param>
+        /// <param name="port_in"></param>
+        /// <returns></returns>
+        public int FindIndex(string IP_IN, short port_in)
+        {
+            int index;
+            try
+            {
+                index = this.Table.FindIndex(r => (r.IP_IN.ToString() == IP_IN) && (r.port_in == port_in));
+                return index;
+            }
+            catch (Exception E)
+            {
+                return -1;
+            }
+        }
+    }
+
+
+}
---
+++ NetworkNode/CommutationField.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkingTools;
+
+namespace NetworkNode
+{
+    /// <summary>
+    /// Klasa reprezentujaca pole komutacyjne.
+    /// </summary>
+    public class CommutationField
+    {
+        //Maksymalny rozmiar bufora wejsciowego
+        public short maxBuffInSize;
+
+        //Maksymalny rozmiar bufora wyjsciowego
+        public short maxBuffOutSize;
+
+        //Bufor wejsciowy
+        public volatile Buffer bufferIn;
+
+        //Lista wyjsciowych buforow
+        public volatile List<Buffer> BuffersOut;
+
+        //Referencja na tablice komutacji brzegowego wezla sieciowego
+        public volatile BorderNodeCommutationTable borderNodeCommutationTable;
+
+        //Referencja na tablice komutacji wezla sieciowego
+        public volatile CommutationTable commutationTable;
+
+        //Referencja na tablice eonowa wezla sieciowego
+        public volatile EONTable EonTable;
+
+
+        public NetworkNode _networkNode;
+
+        /// <summary>
+        /// Konstruktor 
+        /// TODO: Kazdy router ma na razie 3 bufory wyjsciowe
+        /// </summary>
+        public CommutationField()
+        {
+            bufferIn = new Buffer();
+            BuffersOut = new List<Buffer>(3);
+        }
+
+        /// <summary>
+        /// Konstruktor okreslajacy ilosc buforow wyjsciowych
+        /// </summary>
+        /// <param name="buffersOutCount"></param>
+        public CommutationField(int buffersOutCount)
+        {
+            bufferIn = new Buffer();
+            BuffersOut = new List<Buffer>(buffersOutCount);
+        }
+
+        /// <summary>
+        /// Konstruktor z referencjami na tablice i liczba wyjsciowych buforow
+        /// </summary>
+        /// <param name="borderNodeCommutationTable"></param>
+        /// <param name="commutationTable"></param>
+        /// <param name="EonTable"></param>
+        public CommutationField(ref BorderNodeCommutationTable borderNodeCommutationTable,
+            ref CommutationTable commutationTable, ref EONTable EonTable, int buffersOutCount)
+        {
+            bufferIn = new Buffer();
+            BuffersOut = new List<Buffer>(buffersOutCount);
+            this.borderNodeCommutationTable = borderNodeCommutationTable;
+            this.EonTable = EonTable;
+            this.commutationTable = commutationTable;
+        }
+
+        /// <summary>
+        /// Funkcja przetwarzajaca pakiety
+        /// </summary>
+        /// <param name="packageBytes"></param>
+        public async Task<List<Queue<byte[]>>> processPackage(byte[] packageBytes)
+        {
+            //jezeli bufor jest pelny...
+            if (bufferIn.queue.Count == maxBuffInSize)
+            {
+                //Wyprozniamy bufor wejsciowy i zapisujemy jego stan do tego listOfQUeues
+                var listOfQueues = bufferIn.emptyBuffer();
+
+                //do zwrotu wartosci
+                List<Queue<byte[]>> returnListOfQueues;
+
+                //ale listOfQueues powinno miec zawsze 3 kolejki na I etapie...
+                for (int i = 0; i < listOfQueues.Count; i++)
+                {
+                    //Jak jeden pakiet ma ustawiona czestotliwosc na -1, to jest szansa, ze wszystkie maja i 
+                    //Jezeli wszystkie pakiety posiadaja -1 w czestotliwosci
+                    if (Package.extractFrequency(listOfQueues[i].Peek()) == -1 &&
+                        Buffer.checkIfAllPackagesFrequency(listOfQueues[i], -1))
+                    {
+                        listOfQueues[i] = new Queue<byte[]>(_networkNode.borderNodeCommutationTable.
+                            changeHeaderForMessagesFromClient(listOfQueues[i]));
+
+                        //Nowa, zmieniona czestotliwosc. 
+                        //TODO: Na razie jest jedna, a na drugi etap trzeba bedzie przygotowac sortowanie, dzielenie i 
+                        //TODO: dodawanie do konkretnych kolejek
+
+                        //Podpatrujemy na jakas czestotliwosc z pakietu
+                        short freq = Package.extractFrequency(listOfQueues[i].Peek());
+
+                        //sprawdzamy, czy wszystkie czestotliwosci sa takie same
+                        if (Package.extractFrequency(listOfQueues[i].Peek()) == freq &&
+                            Buffer.checkIfAllPackagesFrequency(listOfQueues[i], freq))
+                        {
+                            //szukanie takiej kolejki, ze wierzchni wpis ma interesujaca nas czestotliwosc
+                            var queue = listOfQueues.Find(q => Package.extractFrequency(q.Peek()) == freq);
+
+                            //Gdy nie udalo sie znalezc - kolejka z pakietami o czestotliwosci freq zostaje na swoim miejscu na liscie. Nalezy dodac bufor wyjsciowy do pola
+                            //BuffersOut.Add(new Buffer(listOfQueues[i].Count));
+
+                            //A gdy nie udalo sie znalezc, to dodajemy elementy kolejki z wczesniejszymi "-1" do kolejki z freq
+                            if (queue != null)
+                            {
+                                while (listOfQueues[i].Count > 0)
+                                {
+                                    //Zdejmowanie z jednej kolejki i dopisywanie do drugiej. Nie trzeba znowu sortowac - pakiety z 
+                                    //"-1" maja prawdopodobnie inne pochodzenie
+                                    queue.Enqueue(listOfQueues[i].Dequeue());
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //Rzucamy wyjatek gdy czestotliwosci nie sa takie same
+                            throw new Exception("CommutationField.processPackage(): Zamienione czestotliwosci z -1 na inna nie sa takie same!");
+                        }
+                    }
+
+                    //Kopiowanie z podkolejek do kolejnych buforow wyjsciowych
+                    BuffersOut[i].queue = new Queue<byte[]>(listOfQueues[i]);
+
+                    //sortowanie kolejek wedlug ID
+                    BuffersOut[i].queue = Buffer.sortQueueByID(ref BuffersOut[i].queue);
+
+                    //Lista podkolejek o pakietach o tym samym ID
+                    var listOfSameIDQueues = Buffer.divideSortedQueueByID(ref BuffersOut[i].queue);
+
+                    //Kazda z tych podkolejek sortuje wedlug numeru pakietu
+                    for (int j = 0; j < listOfSameIDQueues.Count; j++)
+                    {
+                        var temp = listOfSameIDQueues[i];
+                        //Sortowanie po numerze pakietu
+                        listOfSameIDQueues[i] = new Queue<byte[]>(Buffer.sortQueueByPackageNumber(ref temp));
+                    }
+
+                    //Sklejanie kolejek z powrotem
+                    var joinedQueue = new Queue<byte[]>(Buffer.joinSortedByPackageNumberQueues(listOfSameIDQueues));
+
+                    //TODO: Zmien to. Trzeba zajrzec w tablice komutacji i skierowac na odpowiedni bufor.
+                    //BuffersOut[i].queue = joinedQueue;
+
+                    //Wpisanie naglowkow do kolejki z pakietami
+                    joinedQueue = commutationTable.changeHeaderForMessagesFromClient(joinedQueue);
+
+                    //Zagladamy na jeden port (wszystkie powinny byc takie same)
+                    short port = Package.extractPortNumber(joinedQueue.Peek());
+
+                    //
+                    switch (port)
+                    {
+                        case 1:
+                            BuffersOut[0].queue = joinedQueue;
+                            break;
+                        case 2:
+                            BuffersOut[1].queue = joinedQueue;
+                            break;
+                        case 3:
+                            BuffersOut[2].queue = joinedQueue;
+                            break;
+                        default:
+                            BuffersOut[0].queue = joinedQueue;
+                            break;
+                    }
+
+                }
+
+                //Tworzenie z buforow listy kolejek i wysterowanie ich na wyjscie
+                returnListOfQueues = new List<Queue<byte[]>>() { BuffersOut[0].queue, BuffersOut[1].queue, BuffersOut[2].queue};
+
+                //wyprozniamy bufor
+                return returnListOfQueues;
+            }
+            else
+            {
+                //dodanie do bufora pakietu
+                bufferIn.queue.Enqueue(packageBytes);
+                return null;
+            }
+        }
+    }
+}
